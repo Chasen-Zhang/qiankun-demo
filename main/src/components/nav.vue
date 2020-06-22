c30411ca8e56d5a62471b137c2eb9a748dff35cf
张森泉 <17324384169@163.com>
2020-06-22 17:48:48 +0800

    remove(删除不相关菜单)

--- main/src/components/nav.vue
+++ main/src/components/nav.vue
@@ -9,7 +9,7 @@
     text-color="#fff"
     active-text-color="#ffd04b"
   >
-    <el-submenu index="1">
+    <!-- <el-submenu index="1">
       <template slot="title">
         <i class="el-icon-location"></i>
         <span>导航一</span>
@@ -20,22 +20,11 @@
         <el-menu-item index="/subapp-marketing">营销菜单一</el-menu-item>
         <el-menu-item index="/subapp-marketing/about">营销菜单二</el-menu-item>
       </el-menu-item-group>
-      <el-submenu index="1-4">
-        <el-menu-item index="1-4-1">选项1</el-menu-item>
-      </el-submenu>
-    </el-submenu>
-    <el-menu-item index="2">
-      <i class="el-icon-menu"></i>
-      <span slot="title">导航二</span>
-    </el-menu-item>
-    <el-menu-item index="3" disabled>
-      <i class="el-icon-document"></i>
-      <span slot="title">导航三</span>
-    </el-menu-item>
-    <el-menu-item index="4">
-      <i class="el-icon-setting"></i>
-      <span slot="title">导航四</span>
-    </el-menu-item>
+    </el-submenu>-->
+    <el-menu-item index="/subapp-logistics">物流菜单一</el-menu-item>
+    <el-menu-item index="/subapp-logistics/about">物流菜单二</el-menu-item>
+    <el-menu-item index="/subapp-marketing">营销菜单一</el-menu-item>
+    <el-menu-item index="/subapp-marketing/about">营销菜单二</el-menu-item>
   </el-menu>
 </template>
 
